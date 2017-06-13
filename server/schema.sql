e443d6a86cf28f1bfd61b67dd346945043057d4e
Hack Reactor Student <student@hackreactor.com>
2017-06-12 19:42:50 -0700

    Initialize the chats database and its tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,31 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int not Null primary key,
+  name varchar(30),
+  room varchar(30),
+  message varchar(140)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE room_message (
+  room_id int not Null primary key,
+  room_name varchar(30),
+  message_id int not Null,
+  foreign key (message_id) references messages (id)
+);
 
+CREATE TABLE name_room (
+  name_id int not Null primary key,
+  name varchar(30),
+  message_id int not Null,
+  foreign key (message_id) references messages (id)
+);
 
 
 
