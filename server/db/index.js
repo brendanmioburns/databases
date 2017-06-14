47bc3388a4ad27ee76f434e5eb66f81415c91a0f
Hack Reactor Student <student@hackreactor.com>
2017-06-13 18:45:08 -0700

    updated models and controllers

--- server/db/index.js
+++ server/db/index.js
@@ -4,18 +4,26 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'plantlife',
-  database : 'chat'
+  host: 'localhost',
+  user: 'root',
+  password: 'plantlife',
+  database: 'chat'
 });
 
-connection.connect()
+connection.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log('You are now connected');
+});
 
-// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-//   if (err) throw err
+connection.query('SELECT * FROM messages', function (err, rows, fields) {
+  if (err) {
+    throw err;
+  }
+  for (var i in rows) {
+    console.log('Messages: ', rows[i].message);
+  }
+});
 
-//   console.log('The solution is: ', rows[0].solution)
-// })
-
-connection.end()
+connection.end();
--- server/models/index.js
+++ server/models/index.js
@@ -2,16 +2,35 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {
-      
+    get: function (cb) {
+      var queryString = 'SELECT * FROM messages';
+
+      db.query(queryString, function(err, results) {
+        cb(err, results);
+      });
     }, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (params, cb) {
+      var queryString = 'INSERT into users(username) values (?)';
+      db.query(queryString, function(err, results) {
+        cb(err, results);
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (cb) {
+      var queryStr = 'select * from users';
+      db.query(queryStr, function(err, results) {
+        cb(err, results);
+      });
+    },
+    post: function (params, cb) {
+      var queryStr = 'insert into users(username) values(?)';
+      db.query(queryStr, params, function(err, results) {
+        cb(err, results);
+      });
+    }
   }
 };
 
